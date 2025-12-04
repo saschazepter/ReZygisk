6179d604e63d04ff747e19bdf367ebbb739b05f0
RainyXeon <rainyxeon@gmail.com>
2025-12-04 13:34:44 +0700

    add: new base framework from TW for new RZ WebUI This commit will push all the basics of TW webui framework to RZ. The current webui still not usable and need some more times to correct, it only contains simple ports from legacy webui.
    List of works I have to do:
    - Modify minipages behavior to match with legacy RZ webui
    - Credits pedro to webui
    - Fix webui won't load when crashing
    - Add some minipages for settings
    - Drop webui error because we can get from ksu debug mode
    - Remove test data
    - Port language file from legacy webui
    
    This framework provided by @ThePedroo so special thanks to him!
    
    Co-authored-by: ThePedroo <pedroolimpioguerra@gmail.com>

--- webroot/js/themes/lightNavbar.js
+++
@@ -1,6 +0,0 @@
-export function setLightNav() {
-  document.getElementById('ni_home').classList.add('light_icon_mode')
-  document.getElementById('ni_modules').classList.add('light_icon_mode')
-  document.getElementById('ni_actions').classList.add('light_icon_mode')
-  document.getElementById('ni_settings').classList.add('light_icon_mode')
-}
