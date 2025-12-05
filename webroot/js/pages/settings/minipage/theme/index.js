e91e23543b367a441e778ab2d6d0bdaf911d5ebd
RainyXeon <rainyxeon@gmail.com>
2025-12-05 12:59:34 +0700

    fix: reapplyListeners() fail to reapply

--- webroot/js/pages/settings/minipage/theme/index.js
+++ webroot/js/pages/settings/minipage/theme/index.js
@@ -1,6 +1,4 @@
-import { whichCurrentPage } from '../../../navbar.js'
-import { getStrings } from '../../../pageLoader.js'
-import { exec, toast } from '../../../../kernelsu.js'
+import { loadPage, setLanguage, reloadPage } from '../../../pageLoader.js'
 
 export async function loadOnce() {
 
@@ -15,5 +13,10 @@ export async function onceViewAfterUpdate() {
 }
 
 export async function load() {
+  const sp_lang_close = document.getElementById('sp_lang_close')
 
+  sp_lang_close.addEventListener('click', async function langCloseButtonListener() {
+    sp_lang_close.removeEventListener('click', langCloseButtonListener)
+    loadPage('settings')
+  })
 }
