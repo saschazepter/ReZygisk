22cf1ab653fbdb9afc80fe3e8faddd4f8495ca00
RainyXeon <rainyxeon@gmail.com>
2025-12-05 14:15:58 +0700

    add: theme, translation, new button and cleanup for WebUI
    - New border for button
    - Ported theme from legacy webui
    - Remove test data
    - Add more translation

--- webroot/js/pages/settings/minipage/theme/index.js
+++ webroot/js/pages/settings/minipage/theme/index.js
@@ -1,4 +1,16 @@
-import { loadPage, setLanguage, reloadPage } from '../../../pageLoader.js'
+import { setThemeData, themeList } from '../../../../themes/main.js'
+import { loadPage } from '../../../pageLoader.js'
+
+async function _setNewThemeIcon() {
+  const back_icon = document.getElementById('sp_theme_close')
+  const sys_theme = localStorage.getItem('/system/theme')
+  if (!sys_theme) return;
+  if (sys_theme == "light") {
+    back_icon.classList.add('light_icon_mode')
+  } else if (back_icon.classList.contains('light_icon_mode')) {
+    back_icon.classList.remove('light_icon_mode')
+  }
+}
 
 export async function loadOnce() {
 
@@ -13,10 +25,25 @@ export async function onceViewAfterUpdate() {
 }
 
 export async function load() {
-  const sp_lang_close = document.getElementById('sp_lang_close')
+  _setNewThemeIcon()
+  const sp_theme_close = document.getElementById('sp_theme_close')
 
-  sp_lang_close.addEventListener('click', async function langCloseButtonListener() {
-    sp_lang_close.removeEventListener('click', langCloseButtonListener)
+  sp_theme_close.addEventListener('click', async function themeCloseButtonListener() {
+    sp_theme_close.removeEventListener('click', themeCloseButtonListener)
     loadPage('settings')
   })
+
+  document.addEventListener('click', async function themeButtonListener(event) {
+    const themeListKey = Object.keys(themeList)
+    const getThemeMode = event.target.getAttribute('theme-data')
+  
+    if (!getThemeMode || typeof getThemeMode !== 'string' || !themeListKey.includes(getThemeMode)) return
+  
+    document.removeEventListener('click', themeButtonListener)
+  
+    themeList[getThemeMode](true)
+  
+    setThemeData(getThemeMode)
+    loadPage('settings')
+  }, false)
 }
